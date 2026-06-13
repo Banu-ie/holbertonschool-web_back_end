356ca2e7544737fa41d69bfae240bf914a69c0d8
Banu-ie <i.banu.4757@gmail.com>
2026-06-13 17:50:05 +0000

    Task 2

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -45,22 +45,20 @@ class Server:
 
         return data[start_index:end_index]
 
-    def get_hyper(self, page: int = 1,
-              page_size: int = 10) -> dict:
-
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
         """Return hypermedia pagination information."""
         data = self.get_page(page, page_size)
 
-        total_pages = math.celi(len(self.dataset()) / page_size)
+        total_pages = math.ceil(len(self.dataset()) / page_size)
 
         next_page = page + 1 if page < total_pages else None
         prev_page = page - 1 if page > 1 else None
 
         return {
-            "page_size" = len(data),
-            "page" = page,
-            "data" = data,
-            "next_page" = next_page,
-            "prev_page" = prev_page,
-            "total_pages" = total_pages
+            "page_size": len(data),
+            "page": page,
+            "data": data,
+            "next_page": next_page,
+            "prev_page": prev_page,
+            "total_pages": total_pages
         }
