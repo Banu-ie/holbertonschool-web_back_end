23191bd6382cf0e0a7f8ea29e53f6b70b398a05f
Banu-ie <i.banu.4757@gmail.com>
2026-06-13 12:03:32 +0000

    Task 0

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
+"""Module providing pagination helper functions."""
 
 def index_range(page, page_size):
+    """Return a tuple containing start and end indexes."""
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
     return (start_index, end_index)
