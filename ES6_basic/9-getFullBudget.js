d2bb26332103a6b48aa8079735abe69ebfc6627f
Banu-ie <i.banu.4757@gmail.com>
2026-06-27 11:38:36 +0000

    Task 9

--- ES6_basic/9-getFullBudget.js
+++ ES6_basic/9-getFullBudget.js
@@ -7,7 +7,7 @@ export default function getFullBudgetObject(income, gdp, capita) {
     ...budget,
     getIncomeInDollars(income) {
       return `$${income}`;
-    }, 
+    },
     getIncomeInEuros(income) {
       return `${income} euros`;
     },
